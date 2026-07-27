0cb78c6ef97dbc3a10229e78df6b083e7749f098
James Clef, MirahSyakilla <mirahsyakilla@gmail.com>
2026-07-28 02:17:01 +0800

    chore: update dependencies
    Signed-off-by: qwq233 <qwq233@qwq2333.top>
    (cherry picked from commit 054e55cf7a13628e57f586fa8f1e7737e1aad436)

--- src/keymaster/db.rs
+++ src/keymaster/db.rs
@@ -1209,9 +1209,8 @@ impl KeystoreDB {
                 return Ok(());
             };
             if !leaf_cert
-                .tbs_certificate
-                .extensions
-                .as_ref()
+                .tbs_certificate()
+                .extensions()
                 .is_some_and(|extensions| {
                     extensions
                         .iter()
@@ -1234,7 +1233,8 @@ impl KeystoreDB {
             let Ok(first_chain_cert) = Certificate::from_der(first_chain_cert) else {
                 return Ok(());
             };
-            if leaf_cert.tbs_certificate.issuer != first_chain_cert.tbs_certificate.subject {
+            if leaf_cert.tbs_certificate().issuer() != first_chain_cert.tbs_certificate().subject()
+            {
                 return Ok(());
             }
 
